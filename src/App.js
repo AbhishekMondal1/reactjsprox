e57ac85092944e7ef9911a814b04560e0270fc48
AbhishekMondal1 <abhishekmondal.official@gmail.com>
2020-09-20 21:56:57 +0530

    State

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import Student from './r.js'
 import './r.css'
 
-function App() {
+class App extends Component {
+  state ={
+    student:[
+      {name:"Jack"},
+      {name:"Bill"},
+      {name:"Mark"},
+    ]
+  }
+  render(){
   return (
     <div>
     <div className="App">
-    <h1>First React Application </h1>
+    <h1>Class Based Components</h1>
     </div>
     <div className="App1">
-    <Student name="Rock"> Top Score </Student>
-    <Student name="Bill"/>
-    <Student name="Mark"/>
-    <Student name="Zoe"/>
+    <Student name={this.state.student[0].name}/>
+    <Student name={this.state.student[1].name}/>
+    <Student name={this.state.student[2].name}/>
     </div>
     </div>
     );
+  }
 }
 
 export default App;
