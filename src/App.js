028aace3adfb5356c9a7235db1d5096834f69ed9
AbhishekMondal1 <abhishekmondal.official@gmail.com>
2020-09-20 22:07:24 +0530

    Hooks

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React,{Component} from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Student from './r.js'
 import './r.css'
 
-class App extends Component {
-  state ={
+function App()  {
+  const [updatedState, changeState] = useState ({
     student:[
       {name:"Jack"},
       {name:"Bill"},
       {name:"Mark"},
     ]
-  }
-  render(){
+  })
+  
   return (
     <div>
     <div className="App">
-    <h1>Class Based Components</h1>
+    <h1>Functional Components</h1>
     </div>
     <div className="App1">
-    <Student name={this.state.student[0].name}/>
-    <Student name={this.state.student[1].name}/>
-    <Student name={this.state.student[2].name}/>
+    <Student name={updatedState.student[0].name}/>
+    <Student name={updatedState.student[1].name}/>
+    <Student name={updatedState.student[2].name}/>
     </div>
     </div>
     );
-  }
 }
 
 export default App;
